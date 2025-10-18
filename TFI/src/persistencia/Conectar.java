62ca08be5bc2bfa9b72c448ecd2af022ec649c1e
Alejandro Peralta <galejandroperalta475@gmail.com>
2025-10-18 18:32:16 -0300

    Clases Conexion y Conectar

---
+++ TFI/src/persistencia/Conectar.java
@@ -0,0 +1,70 @@
+
+package persistencia;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+public class Conectar {
+    
+    public static void main (String[] args){
+        Connection con = null;
+        PreparedStatement ps = null;
+        Cliente cliente = null;
+        ResultSet rs = null;
+        
+        con = Conexion.getConexion();
+        String sql = "INSERT INTO cliente(dni, nombre_completo, telefono, edad, afecciones, estado) VALUES(?,?,?,?,?,?)";
+            
+        try{
+            ps = con.prepareStatement(sql);
+            ps.executeUpdate();
+            
+            sql = "SELECT * FROM cliente";
+            ps = con.prepareStatement(sql);
+            rs = ps.executeQuery();
+            
+            while(rs.next()){
+                cliente = new Cliente();
+                cliente.setcodCli(rs.getInt("codCli"));
+                cliente.setDni(rs.getInt("dni"));
+                cliente.setnombreC(rs.getString("nombre_completo"));
+                cliente.setTelefono(rs.getString("telefono"));
+                cliente.setEdad(rs.getInt("edad"));
+                cliente.setAfecciones(rs.getString("afecciones"));
+                cliente.setEstado(rs.getBoolean("estado"));
+                
+                
+                
+                System.out.println(cliente);
+                
+        }     
+        } catch(SQLException ex){
+            System.out.println("Error" + ex.getMessage());
+        }
+        
+    
+    
+    
+    
+    
+    
+    
+    
+}
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
